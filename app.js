14aaca88b367b228208692e9074ee93d5685582b
Zinnia Rana <zinnia.rana.22@gmail.com>
2020-03-20 22:21:15 -0400

    Event Listener added for click and keypress

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 //MODULES implementation for independent separate module 
 //Data Encapsulation implementation
 
+//Budget Controller
 var budgetController =(function(){
-    var x =23;
-    var add = function(a){
-       return (x+a);
-    }
-
-    return {
-        publicTest: function(b){
-            return (add(b));
-        }
-    }
+    
 })();  //IEFI
 
-var uiController = (function(){
 
+//UI Controller
+var uiController = (function(){
+    var description =  document.querySelector('.add__description').textContent;
 
 })();
 
+//Global App Controller
 var controller = (function(budgetCtrl, uiCtrl){
-    var z= budgetCtrl.publicTest(5);
-    return {
-        publicCtrl: function(){
-            console.log(z);
-        }
-    }
+    var ctrlAddItem = function(){
 
+        console.log("inside");
+        //get the filled input data
+
+        //add item to budget controller
+
+        //add item to UI
+
+        //Calculate the Budget
+    }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13){
+            ctrlAddItem();
+        }
+    })
 })(budgetController, uiController);
